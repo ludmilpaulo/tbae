186a84619a32b510fecc681c7dac53d9af91be9c
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2025-08-06 16:50:32 +0200

    admin

--- backend/venues/admin.py
+++ backend/venues/admin.py
@@ -1,29 +1,38 @@
 from django.contrib import admin
 from .models import Province, Town, Venue, VenueImage
-from adminsortable2.admin import SortableAdminMixin, SortableInlineAdminMixin
 
-class VenueImageInline(SortableInlineAdminMixin, admin.TabularInline):
+# Inline for Venue Images
+class VenueImageInline(admin.TabularInline):
     model = VenueImage
     extra = 1
-    readonly_fields = ("preview",)
+    fields = ('image', 'caption', 'order')
+    readonly_fields = ()
+    ordering = ('order',)
 
-    def preview(self, obj):
-        if obj.image:
-            return f'<img src="{obj.image.url}" style="width:120px;height:auto;"/>'
-        return ""
-    preview.allow_tags = True
-
+# Admin for Venue
 @admin.register(Venue)
-class VenueAdmin(SortableAdminMixin, admin.ModelAdmin):
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'province', 'town', 'price')
+    list_filter = ('province', 'town')
+    search_fields = ('name', 'description', 'details')
     inlines = [VenueImageInline]
-    list_display = ("name", "province", "town")
-    search_fields = ("name", "description", "town__name", "province__name")
 
+# Admin for Province
 @admin.register(Province)
 class ProvinceAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
+    list_display = ('name',)
+    search_fields = ('name',)
 
+# Admin for Town
 @admin.register(Town)
 class TownAdmin(admin.ModelAdmin):
-    list_display = ("name", "province")
-    search_fields = ("name", "province__name")
+    list_display = ('name', 'province')
+    list_filter = ('province',)
+    search_fields = ('name',)
+
+# Admin for VenueImage (optional, mostly managed inline)
+@admin.register(VenueImage)
+class VenueImageAdmin(admin.ModelAdmin):
+    list_display = ('venue', 'caption', 'order')
+    list_filter = ('venue',)
+    search_fields = ('caption',)
--- backend/venues/models.py
+++ backend/venues/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from adminsortable2.admin import SortableInlineAdminMixin
+
 
 
 class Province(models.Model):
